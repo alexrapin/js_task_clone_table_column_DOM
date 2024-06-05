439f31f4dbfbd1994258081e3f898b56dd91615b
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-05 13:20:10 +0300

    make build

---
+++ index.65c5b016.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var t=e.children[1],n=e.children[4],o=e.parentElement,r="";(r="tbody"===o.tagName.toLowerCase()?document.createElement("td"):document.createElement("th")).textContent=t.textContent,n.before(r)});
+//# sourceMappingURL=index.65c5b016.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+// write your code here
+document.querySelectorAll("tr").forEach((tr)=>{
+    const sChild = tr.children[1];
+    const lChild = tr.children[4];
+    const par = tr.parentElement;
+    let nChild = "";
+    if (par.tagName.toLowerCase() === "tbody") nChild = document.createElement("td");
+    else nChild = document.createElement("th");
+    nChild.textContent = sChild.textContent;
+    lChild.before(nChild);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
